40fba6a84d2341c1073875eac464252a77f89be4
Aaron Spence <aaronspence31@gmail.com>
2023-01-14 10:08:19 -0500

    say hello controller

---
+++ src/main/java/com/aaron/todo/todoapp/hello/SayHelloController.java
@@ -0,0 +1,16 @@
+package com.aaron.todo.todoapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+    //"say-hello" => "Hello! How are you?"
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello! How are you?";
+    }
+}
